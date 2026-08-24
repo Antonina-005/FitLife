4f9a3d464209c5895ee9a107e3e4bd1220e7ca98
Antonina-005 <tonic005@yandex.ru>
2026-08-24 19:28:33 +0300

    Update fit_life_Antonina.py

--- fit_life_Antonina.py
+++ fit_life_Antonina.py
@@ -2,6 +2,7 @@
 
 class Training:
     """Базовый класс для тренировки"""
+
     def __init__(self, weight, height):
         self.weight = weight
         self.height = height
@@ -11,12 +12,8 @@ class Training:
         bmi = self.weight / (self.height ** 2)
         return round(bmi, 1)
 
+
 # 1. Сбор данных
-#user_name = input('Как тебя зовут? ')
-#user_age = int(input('Сколько тебе лет? '))
-#user_weight = float(input('Какой у тебя вес в кг? '))
-#user_height = float(input('Какой у тебя рост в метрах 
-#(например, 1.75)? '))
 user_name = 'Antonina'
 user_age = 50
 user_weight = 70.0
@@ -25,10 +22,10 @@ user_height = 1.70
 # 2. Расчеты
 my_training = Training(user_weight, user_height)
 bmi = my_training.calculate_bmi()
-water_needed = user_weight * 30 
+water_needed = user_weight * 30
 
 # 3. Вывод результата
 print(f'Возраст: {user_age} лет')
 print(f'ИМТ: {bmi}')
-print(f'Норма воды: {water_needed} мл') 
+print(f'Норма воды: {water_needed} мл')
 print('Расчет окончен. Будьте здоровы!')

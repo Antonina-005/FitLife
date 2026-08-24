6c0273547235452e46966725f4ab40bf3b839d60
Antonina-005 <tonic005@yandex.ru>
2026-08-24 19:34:37 +0300

    Add files via upload

--- fit_life.py
+++ fit_life.py
@@ -1,26 +1,31 @@
 # Проект FitLife - MVP версия 1.0
 
+class Training:
+    """Базовый класс для тренировки"""
 
-# 1. Знакомство
-# TODO: Спроси у пользователя имя и сохрани в переменную user_name
-# TODO: Спроси возраст и сохрани в переменную user_age (не забудь преобразовать в число)
+    def __init__(self, weight, height):
+        self.weight = weight
+        self.height = height
 
-
-# 2. Сбор данных
-# TODO: Запроси вес (в кг) и сохрани в user_weight (тип float)
-# TODO: Запроси рост (в метрах, например 1.75) и сохрани в user_height (тип float)
-
-
-# 3. Логика расчетов (Функции как "черный ящик": используем арифметику)
-# Формула ИМТ: вес разделить на (рост в квадрате)
-# TODO: Рассчитай bmi (Индекс массы тела)
+    def calculate_bmi(self):
+        """Считает ИМТ и возвращает округлённое значение"""
+        bmi = self.weight / (self.height ** 2)
+        return round(bmi, 1)
 
 
-# Подсчет воды: вес * 30 мл
-# TODO: Рассчитай water_needed
+# 1. Сбор данных
+user_name = 'Antonina'
+user_age = 50
+user_weight = 70.0
+user_height = 1.70
 
+# 2. Расчеты
+my_training = Training(user_weight, user_height)
+bmi = my_training.calculate_bmi()
+water_needed = user_weight * 30
 
-# 4. Вывод красивого результата
-# TODO: Используй f-строку, чтобы вывести приветствие, например: "Привет, Иван!"
-# TODO: Выведи возраст, ИМТ (округленный до 1 знака) и норму воды.
-print("Расчет окончен. Будьте здоровы!")
+# 3. Вывод результата
+print(f'Возраст: {user_age} лет')
+print(f'ИМТ: {bmi}')
+print(f'Норма воды: {water_needed} мл')
+print('Расчет окончен. Будьте здоровы!')
